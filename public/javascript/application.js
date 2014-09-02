639e468bac57f6754905b0266a3e04aaf879c9aa
jpatel531 <jamie@notespublication.com>
2014-09-02 15:17:23 +0100

    Reintroduced angular dependencies

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -3,20 +3,22 @@ var app;
 
 app = angular.module('chitter', ['angularMoment']);
 
-app.controller('ChitterCtrl', function($scope, $http) {
-  var getCheets;
-  $scope.postCheet = function() {
-    return $http.post('/cheets', {
-      cheet: $scope.cheet
-    }).then(function() {
-      getCheets();
-      return $scope.cheet = "";
-    });
-  };
-  getCheets = function() {
-    return $http.get('/cheets').success(function(data) {
-      return $scope.cheets = data;
-    });
-  };
-  return getCheets();
-});
+app.controller('ChitterCtrl', [
+  '$scope', 'http', function($scope, $http) {
+    var getCheets;
+    $scope.postCheet = function() {
+      return $http.post('/cheets', {
+        cheet: $scope.cheet
+      }).then(function() {
+        getCheets();
+        return $scope.cheet = "";
+      });
+    };
+    getCheets = function() {
+      return $http.get('/cheets').success(function(data) {
+        return $scope.cheets = data;
+      });
+    };
+    return getCheets();
+  }
+]);
